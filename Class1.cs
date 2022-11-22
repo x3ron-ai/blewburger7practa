4b040bf3065c183c38453e9494a90a73c709b41b
x3ron-ai <69518463+x3ron-ai@users.noreply.github.com>
2022-11-22 12:13:13 +0300

    pochemu ya negr???

---
+++ Class1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace _7pract
+{
+    public class Cursor
+    {
+        public Cursor(int Min, int Max, int Pos)
+        {
+            min = Min;
+            max = Max;
+            pos = Pos;
+        }
+        public int pos;
+        public int min;
+        public int max;
+    }
+    public class Element
+    {
+        public Element(bool IsFolder, string ElName, string ElPath)
+        {
+            elName = ElName;
+            isFolder = IsFolder;
+            elPath = ElPath;
+        }
+        public string elPath;
+        public bool isFolder;
+        public string elName;
+        public double size;
+    }
+    public static class Functions
+    {
+        private static void AddFile(string path)
+        {
+            File.Create(path);
+        }
+        private static void AddDir(string path)
+        {
+            Directory.CreateDirectory(path);
+        }
+        private static void RemoveFile(string path)
+        {
+            File.Delete(path);
+        }
+        private static void RemoveDir(string path)
+        {
+            // try to catch recursion here!
+            foreach (string file in Directory.GetFiles(path))
+            {
+                File.Delete(file);
+            }
+            foreach (string dir in Directory.GetDirectories(path))
+            {
+                RemoveDir(dir);
+            }
+        }
+        public static void SelectFunc(string directory)
+        {
+            string[] menu = { "Новый файл", "Новая папка", "Удалить файл", "Удалить папку", "Отмена" };
+            Cursor cursor = new Cursor(1, 0, 1);
+            bool exit = false;
+            while (!exit)
+            {
+                cursor.max = 0;
+                Console.WriteLine("Выбор действия");
+                foreach (string func in menu)
+                {
+                    cursor.max++;
+                    Console.WriteLine("   " + func);
+                }
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.DownArrow:
+                        Console.SetCursorPosition(0, cursor.pos);
+                        Console.Write("  ");
+                        if (cursor.pos == cursor.max)
+                            cursor.pos = 1;
+                        else
+                            cursor.pos++;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        Console.SetCursorPosition(0, cursor.pos);
+                        Console.Write("  ");
+                        if (cursor.pos == 1)
+                            cursor.pos = cursor.max;
+                        else
+                            cursor.pos--;
+                        break;
+                    case ConsoleKey.Enter:
+                        switch (cursor.pos)
+                        {
+                            case 1:
+                                Console.Clear();
+                                Console.WriteLine("Имя файла и его расширение: ");
+                                string fileName = Console.ReadLine();
+                                AddFile(directory+"\\"+fileName);
+                                break;
+                        }
+                        break;
+                }
+            }
+        }
+    }
+    public static class GetDirectory
+    {
+        private static Element GetParent(string directory)
+        {
+            DirectoryInfo parent = Directory.GetParent(directory);
+            if (parent != null)
+                return new Element(true, "На уровень выше", parent.FullName);
+            else
+                return new Element(true, "К выбору диска", "\\");
+        }
+        public static List<Element> Files(string directory)
+        {
+            List<Element> elements = new List<Element>();
+            DirectoryInfo parent = Directory.GetParent(directory);
+            elements.Add(GetParent(directory));
+            string[] files = Directory.GetFiles(directory);
+            string[] dirs = Directory.GetDirectories(directory);
+            foreach (string dir in dirs)
+            {
+                Element elem = new Element(true, dir.Split("\\")[^1] + '\\', dir);
+                elements.Add(elem);
+            }
+            foreach (string file in files)
+            {
+                Element elem = new Element(false, file.Split("\\")[^1], file);
+                elements.Add(elem);
+            }
+            return elements;
+        }
+        public static void FileOpenError()
+        {
+            Console.Clear();
+            Console.Write("Ошибка при открытии файла! Окно закроется через 2 секунды");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.Write("Ошибка при открытии файла! Окно закроется через 1 секунду");
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using System.Diagnostics;
+using System.Linq.Expressions;
+using static _7pract.Functions;
+
+namespace _7pract
+{
+    internal class Program
+    {
+        static decimal ConvertToGb(double bytes)
+        {
+            return Math.Round(Convert.ToDecimal(bytes / 1024 / 1024 / 1024), 2);
+        }
+        static void Main(string[] args)
+        {
+            List<Element> elements = new List<Element>();
+            string directory = "\\";
+            while (true)
+            {
+                Console.Clear();
+                if (elements.Count == 0)
+                {
+                    DriveInfo[] allDrives = DriveInfo.GetDrives();
+                    foreach (DriveInfo drive in allDrives)
+                    {
+                        decimal totalSpace = ConvertToGb(drive.TotalSize);
+                        decimal availableSpace = ConvertToGb(drive.AvailableFreeSpace);
+                        Element elem = new Element(true, $"Диск {drive.Name} | Размер {totalSpace} гб | Доступно {availableSpace} гб", drive.Name);
+                        elements.Add(elem);
+                    }
+                }
+                else
+                {
+                    elements = GetDirectory.Files(directory);
+                }
+                bool exit = false;
+                Cursor cursor = new Cursor(1,1,1);
+                while (!exit)
+                {
+                    Console.SetCursorPosition(0, 0);
+                    Console.ResetColor();
+                    Console.Write("Проводник! Директория: ");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(directory);
+                    Console.ResetColor();
+                    cursor.max = 0;
+                    foreach (Element elem in elements)
+                    {
+                        if (elem.isFolder)
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        else
+                            Console.ResetColor();
+                        cursor.max++;
+                        Console.WriteLine("   "+elem.elName);
+                    }
+                    Console.SetCursorPosition(0, cursor.pos);
+                    Console.Write(">>");
+                    switch (Console.ReadKey().Key)
+                    {
+                        case ConsoleKey.DownArrow:
+                            Console.SetCursorPosition(0, cursor.pos);
+                            Console.Write("  ");
+                            if (cursor.pos == cursor.max)
+                                cursor.pos = 1;
+                            else
+                                cursor.pos++;
+                            break;
+                        case ConsoleKey.UpArrow:
+                            Console.SetCursorPosition(0, cursor.pos);
+                            Console.Write("  ");
+                            if (cursor.pos == 1)
+                                cursor.pos = cursor.max;
+                            else
+                                cursor.pos--;
+                            break;
+                        case ConsoleKey.Enter:
+                            if (elements[cursor.pos - 1].isFolder)
+                            {
+                                directory = elements[cursor.pos - 1].elPath;
+                                if (directory == "\\")
+                                    elements.Clear();
+                                exit = true;
+                            }
+                            else
+                            {
+                                try {
+                                    Process.Start(elements[cursor.pos - 1].elPath);
+                                }
+                                catch
+                                {
+                                    GetDirectory.FileOpenError();
+                                }
+                            }
+                            break;
+                        case ConsoleKey.F1:
+                            Functions.SelectFunc(directory);
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
